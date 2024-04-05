cfd48d33ede435e4440a1a9828de9d673e2e3265
Ruslan <Giovingo92@gmail.com>
2024-04-05 21:57:14 +0300

    feat: auth link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Navigation from "./components/navigation";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-9xl p-4">
-        Main Page
-      </h1>
+      <Navigation href="/auth">Auth Page</Navigation>
     </main>
   );
 }
